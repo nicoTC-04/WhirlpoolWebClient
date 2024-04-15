c5d7dd0b247804bf8d6b1dbb8546e0f3dd3be736
nicoTC-04 <nicolastrevinoc@hotmail.com>
2024-04-15 10:22:43 -0600

    'login'

--- whirlpool-web/app/page.tsx
+++ whirlpool-web/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import React, { useState, useEffect } from 'react';
 import Image from "next/image";
 import styles from "./page.module.css";
 import Login from "./Login/page";
@@ -7,9 +9,35 @@ import Game from "./Game/page";
 import Link from "next/link";
 
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Here you could check for user's auth token from localStorage or cookies
+    const token = localStorage.getItem('userToken');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogin = (token: string): void => {
+    localStorage.setItem('userToken', token); // Storing the user token
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('userToken'); // Clearing the user token
+    setIsLoggedIn(false);
+  };
+
   return (
     <>
-    
+      {isLoggedIn ? (
+        <div className={styles.container}>
+          <Dashboard handleLogout={handleLogout} />
+        </div>
+      ) : (
+        <Login handleLogin={handleLogin} />
+      )}
     </>
   );
 }
