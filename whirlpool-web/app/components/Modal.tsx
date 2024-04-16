ac4ff4a74d3cd0c7e3e607ffb521a756e3973581
Pedro San <pedrosava01@gmail.com>
2024-04-16 12:11:06 -0600

    Component for the leaderbard and some UI changes

---
+++ whirlpool-web/app/components/Modal.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "../styles/modal.module.css"
+
+const Modal = () => {
+  return (
+    <div className={styles.container}>
+        <h1> Hola </h1>
+    </div>
+  )
+}
+
+export default Modal
