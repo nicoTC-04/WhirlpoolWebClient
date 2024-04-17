e454470d704156dae7b0acffeefe20d7d3146022
Pedro San <pedrosava01@gmail.com>
2024-04-17 10:08:27 -0600

    scrollview

--- whirlpool-web/app/Login/page.tsx
+++ whirlpool-web/app/Login/page.tsx
@@ -1,93 +1,102 @@
 // login
-'use client'
-import React, { useState } from 'react';
-import Image from 'next/image';
-import styles from './page.module.css';
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 const Login = ({ handleLogin }: { handleLogin: (token: string) => void }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setError('');
+    setError("");
 
     // Validate email and password exists
     if (!email || !password) {
-      setError('Please enter all fields');
+      setError("Please enter all fields");
       return;
     }
 
     setIsLoading(true); // Start loading
-    const apiKey = 'AIzaSyBehaVqkYZZ846B6zMJv2T_40o_bgrG4JU';
+    const apiKey = "AIzaSyBehaVqkYZZ846B6zMJv2T_40o_bgrG4JU";
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
 
     try {
       const response = await fetch(url, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           email: email,
           password: password,
-          returnSecureToken: true
-        })
+          returnSecureToken: true,
+        }),
       });
 
       const data = await response.json();
       setIsLoading(false);
 
       if (!response.ok) {
-        throw new Error(data.error.message || 'An error occurred');
+        throw new Error(data.error.message || "An error occurred");
       }
 
       handleLogin(data.idToken); // Pass the Firebase token to the handler
     } catch (error) {
-      setError((error as Error).message || 'Failed to login');
+      setError((error as Error).message || "Failed to login");
       setIsLoading(false); // End loading
     }
   };
 
   return (
-      <div className={styles.loginContainer}>
-        <div className={styles.logoContainer}>
-          <Image src="/logo_white.png" alt="Whirlpool" width={400} height={133.3} />
-          {/*<Image src="/logo_black.png" alt="Whirlpool" width={400} height={133.3} />*/}
-        </div>
-
-        <div className={styles.separator}></div>
-
-        <form onSubmit={handleSubmit} className={styles.loginForm}>
-          <h2>Login</h2>
-          {error && <p className={styles.error}>{error}</p>}
-          <div className={styles.inputGroup}>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              disabled={isLoading}
-            />
-          </div>
-          <div className={styles.inputGroup}>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              disabled={isLoading}
-            />
-          </div>
-          <button type="submit" className={styles.loginButton} disabled={isLoading}>
-            {isLoading ? 'Logging in...' : 'Login'}
-          </button>
-        </form>
+    <div className={styles.loginContainer}>
+      <div className={styles.logoContainer}>
+        <Image
+          src="/logo_black.png"
+          alt="Whirlpool"
+          width={400}
+          height={133.3}
+        />
+        {/*<Image src="/logo_black.png" alt="Whirlpool" width={400} height={133.3} />*/}
       </div>
+
+      <div className={styles.separator}></div>
+
+      <form onSubmit={handleSubmit} className={styles.loginForm}>
+        <h2>Login</h2>
+        {error && <p className={styles.error}>{error}</p>}
+        <div className={styles.inputGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            disabled={isLoading}
+          />
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={isLoading}
+          />
+        </div>
+        <button
+          type="submit"
+          className={styles.loginButton}
+          disabled={isLoading}
+        >
+          {isLoading ? "Logging in..." : "Login"}
+        </button>
+      </form>
+    </div>
   );
 };
 
