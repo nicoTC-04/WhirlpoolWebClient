9d3563a7d7c59fb5e4fb050fd4aaf5f98e20cc06
nicoTC-04 <nicolastrevinoc@hotmail.com>
2024-04-23 19:53:39 -0600

    fixed config

--- whirlpool-web/next.config.mjs
+++ whirlpool-web/next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Adding ESLint configuration
+    eslint: {
+      // Disabling ESLint checks during production builds
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  export default nextConfig;
+  
