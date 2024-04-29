27d28236755dcd66d9daa85f1b8a5cd591186ab6
nicoTC-04 <nicolastrevinoc@hotmail.com>
2024-04-29 17:29:38 -0600

    final commit

--- whirlpool-web/app/Game/page.tsx
+++ whirlpool-web/app/Game/page.tsx
@@ -1,17 +1,25 @@
 'use client'
-
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
+import { useRouter } from 'next/navigation';
+
 
 const Game = () => {
   const unityUrl = "http://35.188.105.32:3000/";
+
+  const router = useRouter();
+
+  const returnDashboard = () => {
+    console.log("Return dashboard");
+    router.push('/Dashboard');
+  };
   return (
     <>
       <div className={styles.salir}>
         <Link href="/Dashboard">
-          <Image src={"/x-solid.svg"} height={25} width={25} alt="exit"></Image>
+          <Image src={"/x-solid.svg"} height={25} width={25} alt="exit" onClick={returnDashboard}></Image>
         </Link>
       </div>
       <div className={styles.gameDiv}>
